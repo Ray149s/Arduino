c171025845bc5d02408147c8d7fac4e708b3fe7f
Ray149s <rduenas2@csustan.edu>
2022-10-04 10:32:37 -0700

    Commiting 3Diodblinking code.

---
+++ BareMinimum_3DiodBlinking.ino
@@ -0,0 +1,145 @@
+void setup() {
+  // put your setup code here, to run once:
+//Define pin and its type
+pinMode(8,OUTPUT);
+pinMode(6,OUTPUT);
+pinMode(4,OUTPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+//which pin then high or low
+//Red
+digitalWrite(8, HIGH);
+delay(50);
+digitalWrite(8,LOW);
+delay(500);
+digitalWrite(8, HIGH);
+delay(50);
+digitalWrite(8,LOW);
+delay(500);
+digitalWrite(8, HIGH);
+delay(50);
+digitalWrite(8,LOW);
+delay(500);
+digitalWrite(8, HIGH);
+delay(50);
+digitalWrite(8,LOW);
+delay(500);
+digitalWrite(8, HIGH);
+delay(50);
+digitalWrite(8,LOW);
+delay(500);
+
+//Blue
+digitalWrite(6, HIGH);
+delay(50);
+digitalWrite(6,LOW);
+delay(500);
+digitalWrite(6, HIGH);
+delay(50);
+digitalWrite(6,LOW);
+delay(500);
+digitalWrite(6, HIGH);
+delay(50);
+digitalWrite(6,LOW);
+delay(500);
+digitalWrite(6, HIGH);
+delay(50);
+digitalWrite(6,LOW);
+delay(500);
+digitalWrite(6, HIGH);
+delay(50);
+digitalWrite(6,LOW);
+delay(500);
+
+digitalWrite(6, HIGH);
+delay(50);
+digitalWrite(6,LOW);
+delay(500);
+digitalWrite(6, HIGH);
+delay(50);
+digitalWrite(6,LOW);
+delay(500);
+digitalWrite(6, HIGH);
+delay(50);
+digitalWrite(6,LOW);
+delay(500);
+digitalWrite(6, HIGH);
+delay(50);
+digitalWrite(6,LOW);
+delay(500);
+digitalWrite(6, HIGH);
+delay(50);
+digitalWrite(6,LOW);
+delay(500);
+
+//Green
+digitalWrite(4, HIGH);
+delay(50);
+digitalWrite(4,LOW);
+delay(500);
+digitalWrite(4, HIGH);
+delay(50);
+digitalWrite(4,LOW);
+delay(500);
+digitalWrite(4, HIGH);
+delay(50);
+digitalWrite(4,LOW);
+delay(500);
+digitalWrite(4, HIGH);
+delay(50);
+digitalWrite(4,LOW);
+delay(500);
+digitalWrite(4, HIGH);
+delay(50);
+digitalWrite(4,LOW);
+delay(500);
+
+digitalWrite(4, HIGH);
+delay(50);
+digitalWrite(4,LOW);
+delay(500);
+digitalWrite(4, HIGH);
+delay(50);
+digitalWrite(4,LOW);
+delay(500);
+digitalWrite(4, HIGH);
+delay(50);
+digitalWrite(4,LOW);
+delay(500);
+digitalWrite(4, HIGH);
+delay(50);
+digitalWrite(4,LOW);
+delay(500);
+digitalWrite(4, HIGH);
+delay(50);
+digitalWrite(4,LOW);
+delay(500);
+
+
+digitalWrite(4, HIGH);
+delay(50);
+digitalWrite(4,LOW);
+delay(500);
+digitalWrite(4, HIGH);
+delay(50);
+digitalWrite(4,LOW);
+delay(500);
+digitalWrite(4, HIGH);
+delay(50);
+digitalWrite(4,LOW);
+delay(500);
+digitalWrite(4, HIGH);
+delay(50);
+digitalWrite(4,LOW);
+delay(500);
+digitalWrite(4, HIGH);
+delay(50);
+digitalWrite(4,LOW);
+delay(500);
+
+
+
+}
